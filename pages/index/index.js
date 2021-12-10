c09b4c70fa41f62b5d3593112bdba16828361934
chenlei_0903 <chenlei_0903@youzan.com>
2021-12-10 16:43:11 +0800

    feat: 显示水印方案

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,51 @@
+// 添加简单水印
+function addWaterMark(ctx, x, y) {
+  ctx.fillStyle = 'rgba(255, 255 ,255, 1)';
+  ctx.font = "20px Arial";
+  ctx.fillText("Hello World", x, y);
+
+
+  ctx.fillStyle = 'rgba(0, 0 ,0, 1)';
+  ctx.font = "20px Arial";
+  ctx.fillText("Hello World", x, y + 40);
+
+
+  ctx.fillStyle = 'rgba(255, 0 ,0, 1)';
+  ctx.font = "20px Arial";
+  ctx.fillText("Hello World", x, y + 80);
+
+
+  ctx.fillStyle = 'rgba(0, 0 ,255, 1)';
+  ctx.font = "20px Arial";
+  ctx.fillText("Hello World", x, y + 120);
+
+
+  ctx.fillStyle = 'rgba(0, 255 ,0, 1)';
+  ctx.font = "20px Arial";
+  ctx.fillText("Hello World", x, y + 160);
+}
+
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+  const canvas = document.getElementById('canvas');
+  const canvasWaterMark = document.getElementById('canvas-water-mark');
+  const ctx = canvas.getContext("2d");
+  const ctxWaterMark = canvasWaterMark.getContext("2d");
+  const input = document.querySelector("input[type=file]");
+  input.onchange = function () {
+    getImageWH(null, this.files[0], (err, data) => {
+      if (err) return console.error('获取图片框高失败', err)
+      const { width, height, imgElement } = data;
+      canvas.width = width;
+      canvas.height = height;
+      ctx.drawImage(imgElement, 0, 0);
+      addWaterMark(ctx, 200, 0)
+
+      
+      canvasWaterMark.width = width;
+      canvasWaterMark.height = height;
+
+      // 添加简单水印
+      addWaterMark(ctxWaterMark, 0, 0);
+    })
+  }
 }
---
+++ pages/utils.js
@@ -0,0 +1,22 @@
+// 获取图片的框高，支持地址或文件类型
+function getImageWH (src, file, callback) {
+  let currentSrc = null;
+  const img = new Image();
+  if(typeof src === 'string') {
+    currentSrc = src;
+  }
+  if (file instanceof File) {
+    currentSrc = URL.createObjectURL(file);
+  }
+  img.src = currentSrc;
+  img.onload = function() {
+    callback(null, {
+      width: img.width,
+      height: img.height,
+      imgElement: img,
+    })
+  }
+  img.onerror = function(err) {
+    callback(err);
+  }
+}
